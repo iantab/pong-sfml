db9fee2ec147771571e3db98005ab73f0b16f124
iantab <11763662+iantab@users.noreply.github.com>
2026-04-20 23:31:06 -0400

    Pong 2: Drew paddles, ball and score

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,24 @@ int main() {
 	sf::View view(sf::FloatRect({ 0.f, 0.f }, { (float)VIRTUAL_WIDTH, (float)VIRTUAL_HEIGHT }));
 	window.setView(view);
 
+	const sf::Color BG(40, 45, 52);
+
 	sf::Font font("assets/font.ttf");
 	font.setSmooth(false);
 
-	sf::Text text(font, "Hello Pong", 8);
-	text.setFillColor(sf::Color::White);
-	text.setPosition({ VIRTUAL_WIDTH / 2.f - 30, VIRTUAL_HEIGHT / 2.f });
+	sf::Text scoreLeft(font, "0", 32);
+	sf::Text scoreRight(font, "0", 32);
+	scoreLeft.setPosition({ VIRTUAL_WIDTH / 2.f - 50, 30.f });
+	scoreRight.setPosition({ VIRTUAL_WIDTH / 2.f + 30, 30.f });
+
+	sf::RectangleShape leftPaddle({ 5.f, 20.f });
+	leftPaddle.setPosition({ 10.f, VIRTUAL_HEIGHT / 2.f - 10 });
+
+	sf::RectangleShape rightPaddle({ 5.f, 20.f });
+	rightPaddle.setPosition({ VIRTUAL_WIDTH - 15.f, VIRTUAL_HEIGHT / 2.f - 10 });
+
+	sf::RectangleShape ball({ 4.f, 4.f });
+	ball.setPosition({ VIRTUAL_WIDTH / 2.f - 2, VIRTUAL_HEIGHT / 2.f - 2 });
 
 	while (window.isOpen()) {
 		while (const std::optional event = window.pollEvent()) {
@@ -29,8 +41,12 @@ int main() {
 				}
 			}
 		}
-		window.clear(sf::Color::Black);
-		window.draw(text);
+		window.clear(BG);
+		window.draw(leftPaddle);
+		window.draw(rightPaddle);
+		window.draw(ball);
+		window.draw(scoreLeft);
+		window.draw(scoreRight);
 		window.display();
 	}
 }
