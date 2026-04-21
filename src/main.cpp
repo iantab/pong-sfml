33c287e7696134ff3882dedd0d3244099edcf068
iantab <11763662+iantab@users.noreply.github.com>
2026-04-20 20:33:04 -0400

    Pong 1: Added virtual width and height, adjusted font position based on that

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
 #include <SFML/Graphics.hpp>
 #include <optional>
 
+constexpr unsigned VIRTUAL_WIDTH = 432;
+constexpr unsigned VIRTUAL_HEIGHT = 243;
+
 int main() {
 	sf::RenderWindow window(sf::VideoMode({ 1280u, 720u }), "Pong");
 	window.setVerticalSyncEnabled(true);
 
-	sf::Font font("assets/font.ttf");
-	sf::Text text(font, "Hello Pong", 20);
+	sf::View view(sf::FloatRect({ 0.f, 0.f }, { (float)VIRTUAL_WIDTH, (float)VIRTUAL_HEIGHT }));
+	window.setView(view);
 
+	sf::Font font("assets/font.ttf");
+	font.setSmooth(false);
+
+	sf::Text text(font, "Hello Pong", 8);
 	text.setFillColor(sf::Color::White);
-	text.setPosition({ 600.f, 360.f });
+	text.setPosition({ VIRTUAL_WIDTH / 2.f - 30, VIRTUAL_HEIGHT / 2.f });
 
 	while (window.isOpen()) {
 		while (const std::optional event = window.pollEvent()) {
